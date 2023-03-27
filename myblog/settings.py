3172be8858df0b608b84675ae4a369a9be1fb636
YanaSub <61602@pspu.ru>
2023-03-27 19:13:41 +0500

    static, hosts

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-ky0lqj2g-=@te#zd__slgpnx&84g8o5b01o5)f3$3*lrmt$b%v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'www.61602.pythonanywhere.com',
+    '61602.pythonanywhere.com',
+    'pythonanywhere.com'
+]
 
 
 # Application definition
@@ -118,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
